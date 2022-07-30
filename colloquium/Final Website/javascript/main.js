c2d42c23e98dd1171aa3021fdd220d946bc5cd1a
ljc2177 <108085595+ljc2177@users.noreply.github.com>
2022-07-30 17:21:08 -0400

    First draft of colloquium website

---
+++ colloquium/Final Website/javascript/main.js
@@ -0,0 +1,143 @@
+console.log("this works now")
+
+// ------ open modal
+var modal = document.getElementById("popOutBox")
+var btn = document.getElementById("subButton");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+}
+
+// ------ save submissions
+window.addEventListener('load',pageLoadFn)
+let build = {
+    "buildList": []
+}
+
+function pageLoadFn(event) {
+    if(localStorage.getItem('build') === null){
+        return
+    } else {
+        build = JSON.parse(localStorage.getItem('build'))
+        build.buildList.forEach(newBuildPost)
+    }
+}
+
+// ------ additional info restrictions
+const addInfoEle = document.getElementById('add_info');
+const counterEle = document.getElementById('counter');
+
+addInfoEle.addEventListener('input', function(e) {
+    const target = e.target;
+
+    const maxLength = target.getAttribute('maxlength');
+
+    const currentLength = target.value.length;
+
+    counterEle.innerHTML = `${currentLength}/${maxLength}`;
+
+    if (currentLength > 900) {
+        document.getElementById('counter').style.color = 'red';
+        document.getElementById('counter').style.fontWeight = 'bold';
+    } else {
+        document.getElementById('counter').style.color = 'gray';
+        document.getElementById('counter').style.fontWeight = 'normal';
+    }
+});
+
+function resetCounter() {
+    const target = e.target;
+
+    const maxLength = target.getAttribute('maxlength');
+
+    const currentLength = 0;
+};
+
+// ------ set file size restriction
+var uploadField = document.getElementById("file");
+
+uploadField.onchange = function() {
+    if(this.files[0].size > 62914560){
+        alert("This file is too big. Max size is 60MB. Please reduce and re-upload.");
+        this.value = "";
+    };
+};
+
+// ------ post new build to website
+const form = document.querySelector("form");
+const buildPosts = document.querySelector(".newBuildGroup");
+
+const addrInput = document.querySelector("#address");
+const yearInput = document.querySelector("#year");
+const lotInput = document.querySelector("#lot");
+const fileInput = document.querySelector("#file");
+const typeInput = document.querySelector("#use");
+const adInInput = document.querySelector('#add_info');
+
+var button = document.querySelector('button');
+
+// ------ post submissions
+
+const addNewBuild = (e) => {
+    e.preventDefault();
+
+const newAddr = addrInput.value;
+const newYear = yearInput.value;
+const newLot = lotInput.value;
+const newFile = fileInput.value;
+const newType = typeInput.value;
+const newAdIn = adInInput.value;
+
+buildObject = {
+    address: newAddr,
+    year: newYear,
+    lot: newLot,
+    file: newFile,
+    type: newType,
+    additional_info: newAdIn,
+    completed: false,
+}
+
+build.buildList.push(buildObject);
+localStorage.setItem('build', JSON.stringify(build))
+console.log(build)
+newBuildPost();
+document.getElementById("address").focus();
+document.getElementById("address").select();
+alert("Thank you for your submission!");
+
+form.reset();
+counterEle.innerHTML = `0/1000`;
+}
+
+function newBuildPost() {
+    const newBuild = document.createElement("p");
+    newBuild.classList.add("flat");
+    newBuild.style.display = "block";
+    newBuild.innerHTML = 
+        "build address: "+addrInput.value+"<br>"+
+        "build year: "+yearInput.value+"<br>"+
+        "lot number: "+lotInput.value+"<br>"+
+        "build file: "+fileInput.value+"<br>"+
+        "build type: "+typeInput.value+"<br>"+
+        "additional_info: "+adInInput.value;
+    
+        buildPosts.appendChild(newBuild);
+    buildPosts.prepend(newBuild);
+}
+
+form.addEventListener("submit", addNewBuild);
