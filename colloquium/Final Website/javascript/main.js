042103a9933bb9c30a0d77f0c594f8f470cd4684
ljc2177 <108085595+ljc2177@users.noreply.github.com>
2022-07-30 23:50:52 -0400

    add map

--- colloquium/Final Website/javascript/main.js
+++ colloquium/Final Website/javascript/main.js
@@ -1,24 +1,34 @@
 console.log("this works now")
 
-// ------ open modal
-var modal = document.getElementById("popOutBox")
-var btn = document.getElementById("subButton");
+// ------ hide API keys
+const token = config.MAPBOX_TOKEN
+
+// ------ call map
+mapboxgl.accessToken = `${token}`;
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [-73.990593, 40.740121],
+    zoom: 12
+});
+
+// ------ modal map
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("launchMap");
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-    modal.style.display = "none";
+        modal.style.display = "none";
     }
 }
 
