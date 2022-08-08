ed12db6cac0d85e5eabe8008c19d04fe784a0c82
ljc2177 <108085595+ljc2177@users.noreply.github.com>
2022-08-08 14:01:52 -0400

    add artifact markers step 1

--- colloquium/Final Website/javascript/main.js
+++ colloquium/Final Website/javascript/main.js
@@ -49,6 +49,81 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
     modal.style.display = "block";
     map.resize()
+    let marker1 = new mapboxgl.Marker()
+        marker1.setLngLat([-73.982566, 40.690369])
+        marker1.addTo(map)
+
+        let popup1 = new mapboxgl.Popup()
+        popup1.setHTML(
+        "<img src=''>"+
+        "<b>Address:</b> 9 DeKalb Ave, Brooklyn, NY 11201 <br>"+
+        "<b>Anticipated Build Year:</b> 2038 <br>"+
+        "<b>Status: Approved </b><br>"+
+        "<b>Type:</b> Multi-Use: Residential & Commercial <br>"+
+        "<b>Metrics:</b> <br>"+
+        "<b>Representatives in Favor:</b> <br>"+
+        "<b>Representatives Not in Favor:</b>")
+        marker1.setPopup(popup1)
+
+    let marker2 = new mapboxgl.Marker()
+        marker2.setLngLat([-74.00392507369595, 40.75747985782736])
+        marker2.addTo(map)
+
+        let popup2 = new mapboxgl.Popup()
+        popup2.setHTML(
+            "<b>Address:</b> 154 Carroll St <br>"+
+            "<b>Anticipated Build Year:</b> Existing <br>"+
+            "<b>Status:</b> I live here <br>"+
+            "<b>Type:</b> I live here <br>"+
+            "<b>Metrics:</b> I live here <br>"+
+            "<b>Representatives in Favor:</b> I live here <br>"+
+            "<b>Representatives Not in Favor:</b> test")
+        marker2.setPopup(popup2)
+    
+    let marker3 = new mapboxgl.Marker()
+        marker3.setLngLat([-73.97633428852295, 40.75231296743869])
+        marker3.addTo(map)
+
+        let popup3 = new mapboxgl.Popup()
+        popup3.setHTML(
+            "<b>Address:</b> 154 Carroll St <br>"+
+            "<b>Anticipated Build Year:</b> Existing <br>"+
+            "<b>Status:</b> I live here <br>"+
+            "<b>Type:</b> I live here <br>"+
+            "<b>Metrics:</b> I live here <br>"+
+            "<b>Representatives in Favor:</b> I live here <br>"+
+            "<b>Representatives Not in Favor:</b> test")
+        marker3.setPopup(popup3)
+
+    let marker4 = new mapboxgl.Marker()
+        marker4.setLngLat([-74.01128824440441, 40.705644956518185])
+        marker4.addTo(map)
+
+        let popup4 = new mapboxgl.Popup()
+        popup4.setHTML(
+            "<b>Address:</b> 154 Carroll St <br>"+
+            "<b>Anticipated Build Year:</b> Existing <br>"+
+            "<b>Status:</b> I live here <br>"+
+            "<b>Type:</b> I live here <br>"+
+            "<b>Metrics:</b> I live here <br>"+
+            "<b>Representatives in Favor:</b> I live here <br>"+
+            "<b>Representatives Not in Favor:</b> test")
+        marker4.setPopup(popup4)
+
+    let marker5 = new mapboxgl.Marker()
+        marker5.setLngLat([-73.97578662306455, 40.75922487257751])
+        marker5.addTo(map)
+
+        let popup5 = new mapboxgl.Popup()
+        popup5.setHTML(
+            "<b>Address:</b> 154 Carroll St <br>"+
+            "<b>Anticipated Build Year:</b> Existing <br>"+
+            "<b>Status:</b> I live here <br>"+
+            "<b>Type:</b> I live here <br>"+
+            "<b>Metrics:</b> I live here <br>"+
+            "<b>Representatives in Favor:</b> I live here <br>"+
+            "<b>Representatives Not in Favor:</b> test")
+        marker5.setPopup(popup5)
 }
 
 span.onclick = function() {
@@ -60,6 +135,35 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// ------ create map marker list
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+        {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-77.032, 38.913]
+            },
+        properties: {
+            title: 'Mapbox',
+            description: 'Washington, D.C.'
+            }
+        },
+        {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-122.414, 37.776]
+            },
+        properties: {
+            title: 'Mapbox',
+            description: 'San Francisco, California'
+            }
+        }
+    ]
+};
 
 // ------ save submissions
 window.addEventListener('load',pageLoadFn)
